548b98734d6aca3cdf0a1c1b096566806733c2d7
Shuhaib Ahamed <shuhaib.2019678@iit.ac.lk>
2021-03-27 12:59:23 +0530

    ML deployment

--- pikaia/__init__.py
+++ pikaia/__init__.py
@@ -9,14 +9,12 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///pikaia.db'
 app.config['SECRET_KEY'] = 'ec9439cfc6c796ae2029594d'
 db = SQLAlchemy(app)
 
-
 @app.route('/')
 def hello_world():
-    return 'Deployment Success Check!'
-
+    return 'Deployment Success Check! ML model deployed'
 
 from pikaia.admin import routes
-# from pikaia.chatbot import routes
-# from pikaia.music_recommender import routes
+from pikaia.chatbot import routes
+from pikaia.music_recommender import routes
 from pikaia.user import routes
 from pikaia.quotes import routes
