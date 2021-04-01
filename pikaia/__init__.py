84905474b2764473f7fb05945b7d28808b538dc9
Shuhaib Ahamed <shuhaib.2019678@iit.ac.lk>
2021-04-01 12:33:28 +0530

    Backend Deploy Test

--- pikaia/__init__.py
+++ pikaia/__init__.py
@@ -1,42 +1,16 @@
 import pyodbc
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-# import os
 from urllib.parse import quote_plus
 
-# server = os.getenv('pikaia.database.windows.net')
-# database = os.getenv('pikaia')
-# username = os.getenv('pikaia')
-# password = os.getenv('Helloadmin123')
-# port = os.getenv('PORT', default=1433)
-# driver = '{ODBC Driver 17 for SQL Server}'
 
-# connect using parsed URL
-# odbc_str = 'DRIVER='+driver+';SERVER='+server+';PORT='+port+';DATABASE='+database+';UID='+username+';PWD='+password+';'
+# Using pyodbc driver to connect to the cloud database
 drivers = [item for item in pyodbc.drivers()]
 driver = drivers[-1]
 
 
 odbc_str = "DRIVER={"+driver+"};SERVER=pikaia.database.windows.net;DATABASE=pikaia;UID=pikaia;PWD=Helloadmin123;"
 connect_str = 'mssql+pyodbc:///?odbc_connect=' + quote_plus(odbc_str)
-
-# connect with sa url format
-# sa_url = f"mssql+pyodbc://{username}:{password}@{server}:{port}/{database}?driver={driver}"
-
-# SERVER = 'pikaia.database.windows.net'
-# DATABASE = 'pikaia'
-# DRIVER = 'SQL Server'
-# USERNAME = 'pikaia'
-# PASSWORD = 'Helloadmin123'
-# DATABASE_CONNECTION = f'mssql://{USERNAME}:{PASSWORD}@{SERVER}/{DATABASE}?driver={DRIVER}'
-
-
-# engine = create_engine(DATABASE_CONNECTION)
-# connection = engine.connect()
-
-# Configure Database URI:
-# params = urllib.parse.quote_plus("DRIVER={SQL Server};SERVER=pikaia.database.windows.net;DATABASE=pikaia;UID=pikaia;PWD=Helloadmin123;")
-
 
 # initialization
 app = Flask(__name__)
@@ -45,7 +19,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = connect_str
 app.config['SECRET_KEY'] = 'ec9439cfc6c796ae2029594d'
 db = SQLAlchemy(app)
 
-# # # Local Database
+# # Local Database
 # app = Flask(__name__)
 # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///pikaia.db'
@@ -53,6 +27,7 @@ db = SQLAlchemy(app)
 # db = SQLAlchemy(app)
 
 from pikaia.admin import routes
+from pikaia.emotion import routes
 from pikaia.chatbot import routes
 from pikaia.music_recommender import routes
 from pikaia.user import routes
