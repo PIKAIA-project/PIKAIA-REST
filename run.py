b9febccf7a9d22e2f7892553fcf3a5a76dc6040b
Shuhaib Ahamed <shuhaib.2019678@iit.ac.lk>
2021-04-01 19:37:31 +0530

    Views Updated

--- run.py
+++ run.py
@@ -3,3 +3,6 @@ from pikaia import app
 # Checks if the run.py file has executed directly and not imported
 if __name__ == '__main__':
     app.run(debug=True)
+
+    # set FLASK_APP=run.py
+    # flask run
